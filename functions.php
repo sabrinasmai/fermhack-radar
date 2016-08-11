42f51483f464345b5b038863e98f113108349424
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-11 10:57:38 -0400

    adding comment
     Add default posts and comments RSS feed links to head.

--- functions.php
+++ functions.php
@@ -22,8 +22,9 @@ function sabrinas_theme_setup() {
 	 * If you're building a theme based on sabrinas-theme, use a find and replace
 	 * to change 'sabrinas-theme' to the name of your theme in all the template files.
 	 */
-	load_theme_textdomain( 'sabrinas-theme', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'sabrina-theme', get_template_directory() . '/languages' );
 
+	// Add default posts and comments RSS feed links to head.
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
 
