b6881977bf4c8e38e10621420d7e1ad89a79cc13
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-12 10:20:33 -0400

    added sidebar
    https://developer.wordpress.org/reference/functions/is_page/

--- sidebar.php
+++ sidebar.php
@@ -6,12 +6,25 @@
  *
  * @package sabrinas-theme
  */
+ if (!is_page()) { ?>
+ <div id="sidebar" class="widget-area side-widget-area" role="complementary">
+ <?php  if ( is_active_sidebar( 'the_sidebar' ) ) : ?>
+ <div class="sidebar-widgets">
+ <?php  dynamic_sidebar( 'the_sidebar' ); ?>
+ </div>
+ <?php  endif; ?>
+ </div>
+ <?php 
+ 	}
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
-?>
 
-<aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+ 	if ( ! is_active_sidebar( 'sidebar-1' ) && ! is_active_sidebar( 'sidebar-2' ) && ! is_active_sidebar( 'sidebar-3' ) ) {
+ 		return;
+ 	}
+
+ 	?>
+	//The Sidebar containing the area of widget.
+         <?php
+ 	get_sidebar('side');
+
+ 	?>
