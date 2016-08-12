bcb6281358100ce10b1720508afa4ffd8d2a604f
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-12 14:38:30 -0400

    created a content.php
    Template part for displaying single posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package sabrinas-theme
+ * @package sabrina-theme
  */
 
 ?>
@@ -20,7 +20,7 @@
 
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php sabrinas_theme_posted_on(); ?>
+			<?php sabrina_theme_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
@@ -30,18 +30,18 @@
 		<?php
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'sabrinas-theme' ), array( 'span' => array( 'class' => array() ) ) ),
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'sabrina-theme' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sabrinas-theme' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sabrina-theme' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php sabrinas_theme_entry_footer(); ?>
+		<?php sabrina_theme_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
