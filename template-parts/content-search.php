3deeb0cf8e3e6cf5d0963ffb664dbf81f12c182b
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-12 13:51:29 -0400

    sabrina-_theme_posted_on

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package sabrinas-theme
+ * @package sabrina-theme
  */
 
 ?>
@@ -15,7 +15,7 @@
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php sabrinas_theme_posted_on(); ?>
+			<?php sabrina_theme_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -25,6 +25,6 @@
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php sabrinas_theme_entry_footer(); ?>
+		<?php sabrina_theme_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
