e4cc4640e0a9c39037da0667177b126d8de84eea
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-12 13:49:11 -0400

    same comment

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package sabrinas-theme
+ * @package sabrina-theme
  */
 
 ?>
@@ -19,7 +19,7 @@
 			the_content();
 
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sabrinas-theme' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sabrina-theme' ),
 				'after'  => '</div>',
 			) );
 		?>
@@ -31,7 +31,7 @@
 				edit_post_link(
 					sprintf(
 						/* translators: %s: Name of current post */
-						esc_html__( 'Edit %s', 'sabrinas-theme' ),
+						esc_html__( 'Edit %s', 'sabrina-theme' ),
 						the_title( '<span class="screen-reader-text">"', '"</span>', false )
 					),
 					'<span class="edit-link">',
