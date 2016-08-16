e0dfa627cacf06105ca0ab6a9f6efc9ae0ca959e
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-16 19:34:21 -0400

    grid container

--- page_custom.php
+++ page_custom.php
@@ -7,3 +7,19 @@
  */
 
  ?>
+ <?php get_header(); ?>
+
+
+	<div id="gridcontainer">
+		<?php
+
+			$counter = 1;
+			$grid = 2;
+
+			$paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+
+			$args = array('post_type' => 'locations', 'showposts' => 4, 'order' => 'ASC', 'paged' => $paged );
+			$my_query = new WP_Query($args);
+		?>
+
+	<?php
