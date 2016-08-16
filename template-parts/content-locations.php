7f1c4069572a0bfd5f8d394630cdaf03565d5804
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-16 19:04:20 -0400

    entry-meta

--- template-parts/content-locations.php
+++ template-parts/content-locations.php
@@ -6,3 +6,11 @@
  */
 
 ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php sabrina_theme_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
