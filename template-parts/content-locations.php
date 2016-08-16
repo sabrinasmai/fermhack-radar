9a1b466bd9550ad65c5abe4bdb3eff317ad3e1a8
sabrinasmai <sabrina.smai@mail.utoronto.ca>
2016-08-16 19:06:50 -0400

    .entry-content created

--- template-parts/content-locations.php
+++ template-parts/content-locations.php
@@ -14,3 +14,13 @@
 			<?php sabrina_theme_posted_on(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
+
+  	<div class="entry-content">
+  		<?php the_content(); ?>
+  		<?php
+  			wp_link_pages( array(
+  				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'clickture' ),
+  				'after'  => '</div>',
+  			) );
+  		?>
+  	</div><!-- .entry-content -->
